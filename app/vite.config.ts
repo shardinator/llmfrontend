cb633bf51a55edf6f0b8325117909e2c967166bb
Ankur Sharda <sharda.anks@gmail.com>
2026-04-10 07:08:35 +0800

    update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,12 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-      },
-    },
-  },
 })
